737fd6fbd11a99bdc264c438bc7751e0049bc07c
Avinash Kumar Chaubey <avi.kumar.2204@gmail.com>
2020-12-03 00:53:21 +0000

    Id generation

--- pet-clinic-web/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,26 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+	
+	private final OwnerService ownerService;
+	
+	// No need to use Autowired annotation as Spring will automatically autowire 
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
+
 	@RequestMapping({"","/", "/index", "/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }
